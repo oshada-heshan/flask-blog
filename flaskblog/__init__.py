b672dded09c745c0456062774ef770b50573c01c
devoshada <oshadaheshan29@gmail.com>
2026-07-27 22:05:07 +0530

    Blueprints and Configuration

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,19 +5,28 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flaskblog.config import config
 
-app = Flask(__name__)
-app.config.from_object(config)
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-mail = Mail(app)
+mail = Mail()
 
-from flaskblog.users.routes import users
-from flaskblog.posts.routes import posts
-from flaskblog.main.routes import main
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+def create_app(config_class=config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    db.__init__(app)
+    bcrypt.__init__(app)
+    login_manager.__init__(app)
+    mail.__init__(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
